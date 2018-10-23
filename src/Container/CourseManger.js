891a36ec5dda80c85636ac0285fd8a92e9eb7524
dharaksavalia <dharak.savalia@gmail.com>
2018-10-23 13:20:09 -0400

    addition of tabs

--- src/Container/CourseManger.js
+++ src/Container/CourseManger.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
 import CourseCard from '../Component/CourseCard';
 import ModuleList from './ModuleList';
+import LessonTabs from './LessonTabs';
+
 export default class CourseManager extends Component{
     render(){
         return (
             <div className="container-fluid">
+                <LessonTabs></LessonTabs>
                 <ModuleList/>
+
                 <h1>Course Manager</h1>
                 <div className="card-deck">
                     <CourseCard/>
