4ec914c19643a3d61526bbdef55d9f2e5f085160
dharaksavalia <dharak.savalia@gmail.com>
2018-10-23 11:29:53 -0400

    Hello world component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloWorld from './hello';
 
 ReactDOM.render(
-    <h1>Hello, world!</h1>,//jxss
+    <HelloWorld/>,
     document.getElementById('root')
 );
